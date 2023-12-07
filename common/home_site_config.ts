7050abd53c4ecb14c2e7c73115b21d2c68163c8b
LQYld <54627537+LQYld@users.noreply.github.com>
2023-12-07 19:39:36 +0800

    refactor: Extract homePage page configuration parameters

---
+++ common/home_site_config.ts
@@ -0,0 +1,340 @@
+import { fw_logo_map } from './config';
+
+export const members = [
+    {
+        avatar: 'https://avatars.githubusercontent.com/u/54627537',
+        name: 'Qingyun119',
+        title: 'Creator',
+        links: [
+            { icon: 'github', link: 'https://github.com/LQYld' },
+        ]
+    },
+];
+
+export const marginMarko = '0px 8px';
+
+export const web_site = [
+    {
+        icon: fw_logo_map.vue,
+        title: 'Vue',
+        details: 'The Progressive JavaScript Framework',
+        link: 'https://vuejs.org/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.react ,
+        title: 'React',
+        details: 'The library for web and native user interfaces',
+        link: 'https://react.dev/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.svelte,
+        title: 'Svelte',
+        details: 'cybernetically enhanced web apps',
+        link: 'https://svelte.dev/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon:fw_logo_map.solidJS,
+        title: 'SolidJS',
+        details: 'A simple, efficient, and high-performance JavaScript library for building user interfaces',
+        link: 'https://www.solidjs.com/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.qwik,
+        title: 'Qwik',
+        details: 'Deliver instant apps at scale, Build instantly-interactive web apps without effort',
+        link: 'https://qwik.builder.io/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.angular,
+        title: 'Angular',
+        details: 'Deliver web apps with confidence',
+        link: 'https://angular.io/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.lit,
+        title: 'Lit',
+        details: 'Simple · Fast· Web Components',
+        link: 'https://lit.dev/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.ember,
+        title: 'Ember',
+        details: 'A framework for ambitious web developers',
+        link: 'https://emberjs.com/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.alpine,
+        title: 'Alpine',
+        details: 'Your new, lightweight, JavaScript framework',
+        link: 'https://alpinejs.dev/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon:fw_logo_map.aurelia,
+        title: 'Aurelia',
+        details: 'Join thousands creating next-generation apps',
+        link: 'https://aurelia.io/',
+        linkText: 'Go to Official Website',
+    }
+];
+
+export const ssr_site = [
+    {
+        icon: fw_logo_map.nuxt,
+        title: 'NuxtJs',
+        details: 'The Intuitive Vue Framework. Nuxt is an open source framework that makes web development intuitive and powerful. Create performant and production-grade full-stack web apps and websites with confidence',
+        link: 'https://nuxt.com/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.next,
+        title: 'NextJs',
+        details: `The React Framework for the Web. Used by some of the world's largest companies, Next.js enables you to create full-stack Web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds`,
+        link: 'https://nextjs.org/',
+        linkText: 'Go to Official Website',
+    },
+];
+
+export const server_site  = [
+    {
+        icon: fw_logo_map.express,
+        title: 'Express',
+        details: 'Fast, unopinionated, minimalist web framework for Node.js',
+        link: 'https://expressjs.com/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.koa,
+        title: 'Koa',
+        details: 'next generation web framework for node.js',
+        link: 'https://koajs.com/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.fastify,
+        title: 'Fastify',
+        details: 'The Progressive JavaScript Framework',
+        link: 'https://fastify.dev/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.egg,
+        title: 'Egg',
+        details: 'Born to build better enterprise frameworks and apps with Node.js & Koa',
+        link: 'https://www.eggjs.org/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.nest,
+        title: 'NestJs',
+        details: 'The Progressive JavaScript Framework',
+        link: 'https://nestjs.com/',
+        linkText: 'Go to Official Website',
+    },
+];
+
+export const atomicCss_site = [
+    {
+        icon: fw_logo_map.tailwindcss,
+        title: 'Tailwindcss',
+        details: 'Rapidly build modern websites without ever leaving your HTML',
+        link: 'https://tailwindcss.com/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.windicss,
+        title: 'WindiCSS',
+        details: 'Next generation utility-first CSS framework',
+        link: 'https://windicss.org/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.unocss,
+        title: 'UnoCss',
+        details: 'Instant On-demand Atomic CSS Engine',
+        link: 'https://unocss.dev/',
+        linkText: 'Go to Official Website',
+    },
+];
+
+export const cSSPreprocsssor_site = [
+    {
+        icon: fw_logo_map.scss,
+        title: 'Sc/ass',
+        details: 'Sass is the most mature, stable, and powerful professional grade CSS extension language in the world',
+        link: 'https://sass-lang.com/',
+        linkText: 'Go to Official Website',
+    },{
+        icon: fw_logo_map.less,
+        title: 'Less',
+        details: `It's CSS, with just a little more`,
+        link: 'https://lesscss.org/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.stylus,
+        title: 'Stylus',
+        details: 'Expressive, dynamic, and robust CSS. An expressive, robust, feature-rich CSS language built for Node.js',
+        link: 'https://stylus-lang.com/',
+        linkText: 'Go to Official Website',
+    },
+];
+
+export const frontendBuildTooling_site = [
+    {
+        icon: fw_logo_map.esbuild,
+        title: 'Esbuild',
+        details: 'An extremely fast bundler for the web',
+        link: 'https://esbuild.github.io/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.vite,
+        title: 'Vite',
+        details: 'Next Generation Frontend Tooling. Get ready for a development environment that can finally catch up with you',
+        link: 'https://vitejs.dev/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.webpack,
+        title: 'Webpack',
+        details: 'Get Started quickly in our Guides section, or dig into the Concepts section for more high-level information on the core notions behind webpack',
+        link: 'https://webpack.js.org/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.rollup,
+        title: 'Rollup',
+        details: 'The JavaScript module bundler. Compile small pieces of code into something larger and more complex',
+        link: 'https://rollupjs.org/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.turbopack,
+        title: 'Turbopack',
+        details: 'The Rust-powered successor to Webpack. Turbopack is an incremental bundler optimized for JavaScript and TypeScript, written in Rust',
+        link: 'https://turbo.build/pack',
+        linkText: 'Go to Official Website',
+    },
+];
+
+export const packageManagers_site = [
+    {
+        icon: fw_logo_map.npm,
+        title: 'Npm',
+        details: 'Build amazing things',
+        link: 'https://www.npmjs.com/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.pnpm,
+        title: 'Pnpm',
+        details: 'Fast, disk space efficient package manager',
+        link: 'https://pnpm.io/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.yarn,
+        title: 'Yarn',
+        details: 'Safe, stable, reproducible projects',
+        link: 'https://yarnpkg.com/',
+        linkText: 'Go to Official Website',
+    },
+];
+
+export const javaScriptRuntime_site = [
+    {
+        icon: fw_logo_map.node,
+        title: 'NodeJs',
+        details: 'Node.js® is an open-source, cross-platform JavaScript runtime environment',
+        link: 'https://nodejs.org/en',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.deno,
+        title: 'Deno',
+        details: 'Next-generation JavaScript runtime',
+        link: 'https://deno.com/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.bun,
+        title: 'Bun',
+        details: 'Bun is a fast JavaScript test runner & all-in-one toolkit',
+        link: 'https://bun.sh/',
+        linkText: 'Go to Official Website',
+    },
+];
+
+export const cross_platform_desktop_application_site = [
+    {
+        icon: fw_logo_map.electron,
+        title: 'Electron',
+        details: `Build cross-platform desktop apps with JavaScript, HTML, and CSS`,
+        link: 'https://www.electronjs.org/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.tauri,
+        title: 'Tauri',
+        details: `Build an optimized, secure, and frontend-independent application for multi-platform deployment`,
+        link: 'https://tauri.app/',
+        linkText: 'Go to Official Website',
+    },
+];
+
+export const javaScript_testing_site = [
+    {
+        icon: fw_logo_map.mocha,
+        title: 'Mocha',
+        details: `Mocha is a feature-rich JavaScript test framework running on Node.js and in the browser`,
+        link: 'https://mochajs.org/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.jasmine,
+        title: 'Jasmine',
+        details: `Jasmine is a framework for testing JavaScript code`,
+        link: 'https://jasmine.github.io/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.jest,
+        title: 'Jest',
+        details: `Jest is a delightful JavaScript Testing Framework with a focus on simplicity`,
+        link: 'https://jestjs.io/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.vitest,
+        title: 'Vitest',
+        details: `Next Generation Testing Framework A Vite-native testing framework. It's fast`,
+        link: 'https://vitest.dev/',
+        linkText: 'Go to Official Website',
+    },
+];
+
+export const superset_of_javascript = [
+    {
+        icon: fw_logo_map.ts,
+        title: 'TypeScript',
+        details: `TypeScript is a strongly typed programming language that builds on JavaScript, giving you better tooling at any scale`,
+        link: 'https://www.typescriptlang.org/',
+        linkText: 'Go to Official Website',
+    },
+    {
+        icon: fw_logo_map.coffeescript,
+        title: 'CoffeeScript',
+        details: `CoffeeScript is a little language that compiles into JavaScript. Underneath that awkward Java-esque patina, JavaScript has always had a gorgeous heart. CoffeeScript is an attempt to expose the good parts of JavaScript in a simple way`,
+        link: 'https://coffeescript.org/',
+        linkText: 'Go to Official Website',
+    },
+]
